6fb6a44f066a7739e62be5040a3213b6bc87418e
Leonhard Tobisch <leonhard.tobisch@5minds.de>
2016-12-15 17:00:23 +0100

    m
    This is the commit message

---
+++ bootstrapper.el
@@ -0,0 +1,4 @@
+(load-file ".emacs.d/emacs_config/custom.el")
+(load-file ".emacs.d/emacs_config/functions.el")
+(load-file ".emacs.d/emacs_config/keysettings.el")
+(load-file ".emacs.d/emacs_config/hooks.el")
--- keysettings.el
+++ keysettings.el
@@ -1,3 +1,5 @@
 (global-set-key "\C-co" 'switch-to-minibuffer) ;; as defined in functions.el
 
 (global-set-key [C-tab] 'set-rectangular-region-anchor)
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
